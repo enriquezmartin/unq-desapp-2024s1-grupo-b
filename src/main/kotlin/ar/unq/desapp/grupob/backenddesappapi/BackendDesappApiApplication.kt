029b12e46da1478b6169faa58a6416a7bdf2f109
Martin <martin.hernan.enriquez@gmail.com>
2024-03-19 18:29:53 -0300

    Commit inicial proyecto sprint init

---
+++ src/main/kotlin/ar/unq/desapp/grupob/backenddesappapi/BackendDesappApiApplication.kt
@@ -0,0 +1,11 @@
+package ar.unq.desapp.grupob.backenddesappapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendDesappApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendDesappApiApplication>(*args)
+}
