6060ad5001a4ed741b74fbcd27178233ec1c9659
Martin Enriquez <martin.hernan.enriquez@gmail.com>
2024-05-15 19:55:01 -0300

    Add test for getPrices method

--- src/test/kotlin/ar/unq/desapp/grupob/backenddesappapi/service/BinanceApiServiceTest.kt
+++ src/test/kotlin/ar/unq/desapp/grupob/backenddesappapi/service/BinanceApiServiceTest.kt
@@ -29,4 +29,25 @@ class BinanceApiServiceTest {
         assertEquals(10.0F, responsePrice!!.price.toFloat())
         assertEquals("ALICEUSDT", responsePrice.symbol)
     }
+
+    @Test
+    fun testGetPrices() {
+        val symbols = listOf("ALICEUSDT", "ETHBTC")
+
+        val mockResponse = arrayOf(
+            BinancePriceResponse("ALICEUSDT", "10.00"),
+            BinancePriceResponse("ETHBTC", "3000.00")
+        )
+
+        `when`(restTemplateMock.getForObject("https://api1.binance.com/api/v3/ticker/price?symbols=[\"ALICEUSDT\",\"ETHBTC\"]", Array<BinancePriceResponse>::class.java))
+            .thenReturn(mockResponse)
+
+        val prices = binanceApiService.getPrices(symbols)
+
+        assertEquals(2, prices.size)
+        assertEquals("ALICEUSDT", prices[0].symbol)
+        assertEquals("10.00", prices[0].price)
+        assertEquals("ETHBTC", prices[1].symbol)
+        assertEquals("3000.00", prices[1].price)
+    }
 }
