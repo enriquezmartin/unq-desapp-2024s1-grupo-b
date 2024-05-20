f98f356ff64e993edd56116c9a61fa6c86d90f7d
Martin Enriquez <martin.hernan.enriquez@gmail.com>
2024-05-20 01:04:58 -0300

    Start adding data loaders for users, this will run at startup and create initial data

---
+++ src/main/kotlin/ar/unq/desapp/grupob/backenddesappapi/dataInit/UserDataLoader.kt
@@ -0,0 +1,68 @@
+package ar.unq.desapp.grupob.backenddesappapi.dataInit
+
+import ar.unq.desapp.grupob.backenddesappapi.model.UserEntity
+import ar.unq.desapp.grupob.backenddesappapi.repository.UserRepository
+import org.springframework.boot.CommandLineRunner
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
+
+@Configuration
+class UserDataLoader(private val userRepository: UserRepository) {
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    //se creara un cmd line runner por cada entidad, y se debe tener en cuenta el orden en que se ejecutan
+    fun initUsers()= CommandLineRunner {
+        val user0 = UserEntity(
+            "foodlover53@aol.com",
+            "P4ss*_w0rd",
+            "Homero",
+            "Simpson",
+            "Evergreen avenue 742",
+            "2613984763127892345314",
+            "87654321"
+        )
+
+        val user1 = UserEntity(
+            "goat10@messi.com",
+            "Messi_4life!",
+            "Lionel",
+            "Messi",
+            "Rosario street 10",
+            "1234567890123456789012",
+            "10101010"
+        )
+
+        val user2 = UserEntity(
+            "spidey@dailybugle.com",
+            "Web*_l1nger23",
+            "Peter",
+            "Parker",
+            "20 Ingram Street",
+            "2345678901234567890123",
+            "20202020"
+        )
+
+        val user3 = UserEntity(
+            "saiyan.kakarot@dbz.com",
+            "Dr@g0n%BallZ",
+            "Goku",
+            "Son",
+            "Mount Paozu",
+            "3456789012345678901234",
+            "30303030"
+        )
+
+        val user4 = UserEntity(
+            "wizarding.world@hogwarts.com",
+            "Magic!_W4nd",
+            "Harry",
+            "Potter",
+            "4 Privet Drive",
+            "4567890123456789012345",
+            "40404040"
+        )
+        userRepository.saveAll(listOf(user0, user1, user2, user3, user4))
+    }
+}
