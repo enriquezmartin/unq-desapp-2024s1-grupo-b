2a9056f3f5afc552d4aee9eaff288b7c07a2e039
ariel1105 <73146826+ariel1105@users.noreply.github.com>
2024-06-25 11:47:30 -0300

    testing operation service

--- src/test/kotlin/ar/unq/desapp/grupob/backenddesappapi/service/CryptoOperationServiceTest.kt
+++ src/test/kotlin/ar/unq/desapp/grupob/backenddesappapi/service/CryptoOperationServiceTest.kt
@@ -2,9 +2,9 @@ package ar.unq.desapp.grupob.backenddesappapi.service
 
 import ar.unq.desapp.grupob.backenddesappapi.helpers.OperationBuilder
 import ar.unq.desapp.grupob.backenddesappapi.helpers.PostBuilder
+import ar.unq.desapp.grupob.backenddesappapi.helpers.PriceBuilder
 import ar.unq.desapp.grupob.backenddesappapi.helpers.UserBuilder
-import ar.unq.desapp.grupob.backenddesappapi.model.OperationStatus
-import ar.unq.desapp.grupob.backenddesappapi.model.PostStatus
+import ar.unq.desapp.grupob.backenddesappapi.model.*
 import ar.unq.desapp.grupob.backenddesappapi.repository.CryptoOperationRepository
 import ar.unq.desapp.grupob.backenddesappapi.repository.PostRepository
 import ar.unq.desapp.grupob.backenddesappapi.repository.PriceRepository
@@ -18,6 +18,8 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
 import java.util.*
 @ExtendWith(SpringExtension :: class)
 @SpringBootTest
@@ -37,76 +39,81 @@ class CryptoOperationServiceTest {
     private lateinit var postRepository: PostRepository
 
     @MockBean
-    private lateinit var cryptoOperationRepository: CryptoOperationRepository
+    private lateinit var operationRepository: CryptoOperationRepository
 
 
-//    @Test
-//    fun `Payout notification success`(){
-//        var client = UserBuilder().withId(1L).build()
-//        var post = PostBuilder().withId(1L).build()
-//        `when`(userRepository.findById(client.id!!)).thenReturn(Optional.of(client))
-//        `when`(postRepository.findById(post.id!!)).thenReturn(Optional.of(post))
-//
-//        val operation = service.payoutNotification(post.id!!, client.id!!)
-//
-//        assertEquals(operation.status, OperationStatus.IN_PROGRESS)
-//        assertEquals(operation.post!!.status, PostStatus.IN_PROGRESS)
-//        assertEquals(operation.client!!.id, client.id)
-//
-//    }
+    @Test
+    fun payoutNotificationTest(){
+        val currentTime: LocalDateTime = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS)
+        val client: UserEntity = UserBuilder().withId(1L).build()
+        val owner: UserEntity = UserBuilder().withId(2L).build()
+        val post: Post = PostBuilder().withId(1L).withOperationType(OperationType.PURCHASE).withStatus(PostStatus.ACTIVE).withPrice(10f).withUser(owner).build()
+        val price: Price = PriceBuilder().withValue(10f).build()
 
-    /*@Test
-    fun `when price is out of range for a post in payout notification the post become cancelled`(){
-        var client = UserBuilder().withId(1L).build()
-        var post = PostBuilder()
-            .withId(1L)
-            .withOperationType(OperationType.PURCHASE)
-            .withCryptoCurrency(CryptoCurrency.AAVEUSDT)
-            .withPrice(10F)
-            .build()
-        var price = PriceBuilder()
-            .withValue(15F)
-            .build()
-
-        `when`(userRepository.findById(client.id!!)).thenReturn(Optional.of(client))
-        `when`(postRepository.findById(post.id!!)).thenReturn(Optional.of(post))
+        `when`(postRepository.findById(1L)).thenReturn(Optional.of(post))
+        `when`(userRepository.findById(1L)).thenReturn(Optional.of(client))
         `when`(priceRepository.findFirstByCryptoCurrencyOrderByPriceTimeDesc(post.cryptoCurrency!!)).thenReturn(price)
 
         val operation = service.payoutNotification(post.id!!, client.id!!)
 
-        assertEquals(operation.status, OperationStatus.CANCELLED)
-        assertEquals(operation.post!!.status, PostStatus.ACTIVE)
-    }*/
+        assertEquals(operation.status, OperationStatus.IN_PROGRESS)
+        assertEquals(operation.dateTime, currentTime)
+        assertEquals(operation.post!!.status, PostStatus.IN_PROGRESS)
+    }
 
-    /*@Test
-    fun `confirm`(){
-        var owner = UserBuilder()
-            .withId(1L)
-            .build()
-        var client = UserBuilder()
-            .build()
+    @Test
+    fun confirmTest(){
+        var client: UserEntity = UserBuilder().withId(1L).withSuccesfulOperations(0).build()
+        var owner: UserEntity = UserBuilder().withId(2L).withScore(0).withSuccesfulOperations(0).build()
         var post = PostBuilder()
             .withStatus(PostStatus.IN_PROGRESS)
+            .withUser(owner)
             .build()
-        var operation = OperationBuilder()
-            .withStatus(OperationStatus.IN_PROGRESS)
-            .withPost(post)
-            .withClient(client)
-            .withId(1L)
+        val operation =
+            OperationBuilder()
+                .withId(1L)
+                .withDateTime(LocalDateTime.now())
+                .withStatus(OperationStatus.IN_PROGRESS)
+                .withPost(post)
+                .withClient(client)
+                .build()
+
+        `when`(userRepository.findById(1L)).thenReturn(Optional.of(client))
+        `when`(userRepository.findById(2L)).thenReturn(Optional.of(owner))
+        `when`(operationRepository.findById(1L)).thenReturn(Optional.of(operation))
+
+
+        val result = service.confirmOperation(owner.id!!, operation.id!!)
+
+        assertEquals(result.post!!.owner!!.score, 10)
+    }
+
+    @Test
+    fun cancelTest(){
+        var client: UserEntity = UserBuilder().withId(1L).withScore(20).build()
+        var owner: UserEntity = UserBuilder().withId(2L).build()
+        var post = PostBuilder()
+            .withStatus(PostStatus.IN_PROGRESS)
+            .withUser(owner)
             .build()
+        val operation =
+            OperationBuilder()
+                .withId(1L)
+                .withDateTime(LocalDateTime.now())
+                .withStatus(OperationStatus.IN_PROGRESS)
+                .withPost(post)
+                .withClient(client)
+                .build()
 
-        `when`(cryptoOperationRepository.findById(operation.id!!)).thenReturn(Optional.of(operation))
-        `when`(userRepository.findById(owner.id!!)).thenReturn(Optional.of(owner))
+        `when`(userRepository.findById(1L)).thenReturn(Optional.of(client))
+        `when`(userRepository.findById(2L)).thenReturn(Optional.of(owner))
+        `when`(operationRepository.findById(1L)).thenReturn(Optional.of(operation))
 
-        service.confirmOperation(owner.id!!, operation.id!!)
 
-        assertEquals(owner.score,10)
-        assertEquals(post.status, PostStatus.CLOSED)
-        assertEquals(operation.status, OperationStatus.CLOSED)
-        assertEquals(client.score,10)
-        assertEquals(owner.successfulOperation, 1)
-        assertEquals(client.successfulOperation, 1)
+        val result = service.cancelOperation(client.id!!, operation.id!!)
 
-    }*/
+        assertEquals(result.client!!.score, 0)
+    }
+
 
 }
